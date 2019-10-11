9f428f2a598ce834047bec7adc246212635f9d3b
bmcandrews11 <bmcandrews11@gmail.com>
2019-10-11 11:48:25 -0400

    started func

--- FinalGradeCalculator/ViewController.swift
+++ FinalGradeCalculator/ViewController.swift
@@ -8,9 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    var myGrade = 0.0
+    var desiredFinalGrade = 0.0
+    var examPercentage = 0.2
+    var exam = 0.0
+    var hold1 = 0.0
+    var hold2 = 0.0
+    
+    func caculate(num1: Double, num2: Double, num3: Double)
+    {
+        myGrade = num1
+        desiredFinalGrade = num2
+        examPercentage = num3
+        hold1 = myGrade * 0.8
+        //hold2 = * examPercentage
+        
+        
+        
+        
+    }
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
