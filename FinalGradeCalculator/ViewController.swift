c63ad095153fffae8114e6b00d1b0ce7de28bee4
WhyDoYouStare <tjaidon2@gmail.com>
2019-10-11 11:48:45 -0400

    Tap Gesture with other Function to the buttons

--- FinalGradeCalculator/ViewController.swift
+++ FinalGradeCalculator/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myTextField1: UITextField!
+    @IBOutlet weak var myTextField2: UITextField!
+    @IBOutlet weak var myTextField3: UITextField!
+    @IBOutlet weak var myTextField4: UITextField!
+    @IBOutlet weak var myTextField5: UITextField!
+    @IBOutlet weak var myTextField6: UITextField!
+    
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapGestureRec(_ sender: UITapGestureRecognizer) {
+   
+    
+    
+    
+    }
+    
 }
 
